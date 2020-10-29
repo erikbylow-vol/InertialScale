079561ba496ff40a50076e656dfa0f448d57df2a
erikbylow <erik.bylow@volumental.com>
2020-10-29 16:17:29 +0100

    getNewJsonWithScale: Rescale pose and save in json

--- code/getNewJsonWithScale.m
+++ code/getNewJsonWithScale.m
@@ -2,5 +2,12 @@ function json = getNewJsonWithScale(old_json, scale)
     json = old_json;
     for k = 1:length(json)
         json(k).scale = scale;
+        similarity_transform = eye(4)*scale;
+        similarity_transform(4, 4) = 1;
+        world_from_camera = reshape(json(k).world_from_camera,[4, 4])';
+        world_from_camera(:,4) = similarity_transform * world_from_camera(:,4);
+        world_from_camera = world_from_camera';
+        json(k).world_from_camera = world_from_camera(:);
+        
     end
 end
